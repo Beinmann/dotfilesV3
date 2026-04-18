ea5abd4d20b631c871277ac0d837e5997ba62ae1
Beinmann <Beinmann@proton.me>
2026-04-18 05:58:50 +0100

    feat(i3blocks): update the audio icon

--- modules/i3/.config/i3/scripts/volume_status.sh
+++ modules/i3/.config/i3/scripts/volume_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LONG_FORMAT='${SYMB} ${VOL}%'
-export AUDIO_HIGH_SYMBOL="<span font='FontAwesome'></span>"
-export AUDIO_MED_SYMBOL="<span font='FontAwesome'></span>"
-export AUDIO_LOW_SYMBOL="<span font='FontAwesome'></span>"
-export AUDIO_MUTED_SYMBOL="<span font='FontAwesome'></span>"
+export AUDIO_HIGH_SYMBOL="<span font='FontAwesome'>"$'\uf028'"</span>"
+export AUDIO_MED_SYMBOL="<span font='FontAwesome'>"$'\uf027'"</span>"
+export AUDIO_LOW_SYMBOL="<span font='FontAwesome'>"$'\uf026'"</span>"
+export AUDIO_MUTED_SYMBOL="<span font='FontAwesome'>"$'\uf026'$'\u20e0'"</span>"
 exec "$HOME/.config/i3blocks/scripts/volume-pipewire/volume-pipewire" -S
