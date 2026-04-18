3e5203567bc6fd9503606aba1978e17aa2f4b874
Beinmann <Beinmann@proton.me>
2026-04-18 05:46:09 +0100

    feat(i3blocks): another attempt at making a volume block

---
+++ modules/i3/.config/i3/scripts/volume_status.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export LONG_FORMAT='${SYMB} ${VOL}%'
+export AUDIO_HIGH_SYMBOL="<span font='FontAwesome'></span>"
+export AUDIO_MED_SYMBOL="<span font='FontAwesome'></span>"
+export AUDIO_LOW_SYMBOL="<span font='FontAwesome'></span>"
+export AUDIO_MUTED_SYMBOL="<span font='FontAwesome'></span>"
+exec "$HOME/.config/i3blocks/scripts/volume-pulseaudio/volume-pulseaudio" -S
