005489d408d01607961dc7926c83239f1d9cb3db
Beinmann <Beinmann@proton.me>
2026-04-18 04:09:49 +0100

    chore: remove temporary scripts

--- modules/base/Main/Additional_Config/Temporary_Scripts/Bacheloranfangspraesentation_latex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-path=~/Main/Uni/SoSe2024/Bachelorarbeit/BachelorAnfangsPraesentation/beamer-template
-
-latexmk --interaction=nonstopmode --halt-on-error
-
-pkill xournalpp
-
-open "$path/slides.pdf" &
